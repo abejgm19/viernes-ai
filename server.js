730c35ec353ed988e1cbab10424b755063fe315c
abejgm19 <abelg6468@gmail.com>
2026-02-26 17:02:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Viernes está funcionando");
 });
 
+app.post("/chat", (req, res) => {
+  const mensaje = req.body.mensaje;
+
+  res.json({
+    respuesta: "Hola, soy Viernes. Recibí tu mensaje: " + mensaje
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Servidor corriendo en puerto " + PORT);
+  console.log("Servidor de Viernes activo");
 });
